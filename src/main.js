7dbb72ec17312745f303beb373101bc6479a24b3
ofaaoficial <oscarfamado@gmail.com>
2022-03-26 20:15:49 -0500

    feat: name for routes

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
-import LoginPage from './pages/public/LoginPage'
-import RegisterPage from './pages/public/RegisterPage'
-import DashboardPage from './templates/DashboardTemplate'
 import ReportsPage from "@/pages/dashboard/ReportsPage";
 import FamilyPage from "@/pages/dashboard/FamilyPage";
 import HealthPage from "@/pages/dashboard/HealthPage";
+import VueGoogleCharts from 'vue-google-charts'
+import CalendarPage from "@/pages/dashboard/CalendarPage";
+import LoginPage from "@/pages/public/LoginPage";
+import RegisterPage from "@/pages/public/RegisterPage";
+import DashboardTemplate from "@/templates/DashboardTemplate";
 
+Vue.use(VueGoogleCharts)
 Vue.use(VueRouter);
 Vue.config.productionTip = false
 
@@ -17,30 +20,41 @@ const router = new VueRouter({
     routes: [
         {
             path: '/',
-            redirect: '/ingresar'
+            redirect: {name: 'login'}
         },
         {
             path: '/ingresar',
+            name: 'login',
             component: LoginPage
         },
         {
             path: '/registrarme',
+            name: 'register',
             component: RegisterPage
         },
         {
             path: '/tablero',
-            component: DashboardPage,
+            name: 'dashboard',
+            component: DashboardTemplate,
             children: [
                 {
+                    path: '',
+                    name: 'calendar',
+                    component: CalendarPage
+                },
+                {
                     path: 'salud',
+                    name: 'health',
                     component: HealthPage
                 },
                 {
                     path: 'reportes',
+                    name: 'reports',
                     component: ReportsPage
                 },
                 {
                     path: 'familia',
+                    name: 'family',
                     component: FamilyPage
                 },
 
