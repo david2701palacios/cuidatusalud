94347123adfa2a2d17b776d5dc0175013a7af1a8
ofaaoficial <oscarfamado@gmail.com>
2022-03-26 20:31:26 -0500

    move: move frontend project

---
+++ frontend/src/components/tables/UsersTable.vue
@@ -0,0 +1,74 @@
+<template>
+    <section>
+        <v-data-table
+            :headers="headers"
+            :items="users"
+            :items-per-page="5"
+            class="elevation-1"
+            :search="search"
+            :custom-filter="filterOnlyCapsText"
+        >
+            <template v-slot:top>
+                <v-text-field
+                    v-model="search"
+                    label="Buscar"
+                    class="mx-4"
+                ></v-text-field>
+            </template>
+        </v-data-table>
+    </section>
+</template>
+<script>
+export default {
+    name: 'UsersTable',
+    data() {
+        return {
+            search: '',
+            headers: [
+                {
+                    text: 'Nombre',
+                    align: 'start',
+                    value: 'fullName',
+                },
+                {text: 'Correo electrónico', value: 'email'},
+                {text: 'Genero', value: 'gender'},
+                {text: 'Edad', value: 'age'},
+            ],
+            users: [
+                {
+                    fullName: 'Oscar Amado',
+                    email: 'oscarfamado@gmail.com',
+                    gender: 'Masculino',
+                    age: '18',
+                },
+                {
+                    fullName: 'Kerttu Wallo',
+                    email: 'kerttu.wallo@example.com',
+                    gender: 'Femenino',
+                    age: '23',
+                },
+                {
+                    fullName: 'Evaldo Nascimento',
+                    email: 'evaldo.nascimento@example.com',
+                    gender: 'Masculino',
+                    age: '18',
+                },
+                {
+                    fullName: 'Erin Anderson',
+                    email: 'erin.anderson@example.com',
+                    gender: 'Masculino',
+                    age: '42',
+                },
+            ],
+        }
+    },
+    methods: {
+        filterOnlyCapsText(value, search) {
+            return value &&
+                search &&
+                typeof value === 'string' &&
+                value.toString().toLowerCase().includes(search.toLowerCase())
+        },
+    },
+}
+</script>
---
+++ frontend/src/pages/dashboard/FamilyPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <BasicPageTemplate title="Familia">
+        <UsersTable/>
+    </BasicPageTemplate>
+</template>
+<script>
+import BasicPageTemplate from "@/templates/BasicPageTemplate";
+import UsersTable from "@/components/tables/UsersTable";
+
+export default {
+    name: 'FamilyPage',
+    components: {BasicPageTemplate, UsersTable}
+}
+</script>
